79aa949352fb883f8e7cc651cf3f1043ba91e111
= <kris.borrmann@gmail.com>
2020-07-28 16:52:50 +0200

    2.1 Kommentare und kleine Änderungen

--- AB/AB2_1.hs
+++ AB/AB2_1.hs
@@ -11,14 +11,17 @@ primitiveSort :: (Ord a) => [a] -> [a]
 
 -- *** DO NOT MODIFY ABOVE CODE ***
 
+--erstellt eine Liste mit allen indizes und filtert diese wie oben angegeben
 unorderedWitnesses a = filter f [0..((length a) -2)]
     where
         f x = (get (x+1) a) < (get (x) a)
 
+-- da set die geänderte Liste ausgibt, wird mit set(set()) zwei Änderungen hintereinander realisiert
 swap i j a = set (get j a) i (set (get i a) j a)
 
 primitiveSort a = recSort a 0 (unorderedWitnesses a)
+-- swapt rekursiv die Liste an den Stellen der Witnesses. Läuft mehrfach durch, bis keine Witnesses mehr vorhanden sind
 recSort a i w = if' (i < length w) goThroughWitnesses untilWitnessesEmpty
     where 
-        untilWitnessesEmpty = (if' (0 < length w) (recSort a 0 (unorderedWitnesses a)) a)
-        goThroughWitnesses = (recSort (swap (get i w) ((get i w) + 1) a) (i + 1) (unorderedWitnesses a))
+        goThroughWitnesses = recSort (swap (get i w) ((get i w) + 1) a) (i + 1) w --swapt alle witnesses, solange welche da sind
+        untilWitnessesEmpty = if' (0 < length w) (recSort a 0 (unorderedWitnesses a)) a --startet das swappen von vorn, sofern es noch unordered witnesses gibt
