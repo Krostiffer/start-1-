f1524841328f7dc1f70c56b56e8279ec67ea9af2
Leon <leonolmo.lecht@gmail.com>
2020-07-28 16:31:59 +0200

    AB2_2 add, mult, pow

--- AB/AB2_2.hs
+++ AB/AB2_2.hs
@@ -11,7 +11,20 @@ genericArithmetic :: (Int -> Int) -> (Int -> Int -> Int) -> Int -> Int -> Int
 
 -- *** DO NOT MODIFY ABOVE CODE ***
 
-add = undefined
-mult = undefined
-pow = undefined
+-- as long as y is not null add 1 to the result and sub 1 from y
+add x y = if' (y == 0) (x) (add (succ x) (pred y))
+
+-- we need a third value storing the value that is to be added in the recursion
+multHelp :: Int -> Int -> Int -> Int
+multHelp x y a = if' ((pred y) == 0) (x) (multHelp (add a x) (pred y) a) 
+
+-- precheck  if x or y are 0 as addition cannot handle this correctly (only if both are 0)
+mult x y = if' (x == 0) 0 (if' (y == 0) 0 (multHelp x y x))
+
+powHelp :: Int -> Int -> Int -> Int
+powHelp x y m = if' ((pred y) == 0) (x) (powHelp (mult x m) (pred y) m) 
+
+-- precheck if y == 0 (x to the power of 0) to result in 1 as the multiplication can only handle if x == 0
+pow x y =  if' (y == 0) 1 (powHelp x y x)
+
 genericArithmetic = undefined
