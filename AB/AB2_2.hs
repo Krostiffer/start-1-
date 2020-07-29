dfb3dc7dca722eb6af7729249639d15e4e3c9940
= <kris.borrmann@gmail.com>
2020-07-29 14:22:55 +0200

    2.3 (und 2.2 Kommentare)

--- AB/AB2_2.hs
+++ AB/AB2_2.hs
@@ -12,11 +12,11 @@ genericArithmetic :: (Int -> Int) -> (Int -> Int -> Int) -> Int -> Int -> Int
 -- *** DO NOT MODIFY ABOVE CODE ***
 
 add x y = (genericArithmetic id addhelp) x y
-addhelp x y = add (succ x) (pred y)
+addhelp x y = add (succ x) (pred y) 
 
 mult x y = (genericArithmetic pred multhelp) x y
-multhelp x y = if' (y == 0) 0 (add (x) (mult x (pred y)))
+multhelp x y = if' (y == 0) 0 (add (x) (mult x (pred y))) -- a * b = a+(a*(b-1))
 
 pow x y = (genericArithmetic pred powhelp) x y
-powhelp x y = if' (y == 0) 1 (mult (x) (pow x (pred y)))
+powhelp x y = if' (y == 0) 1 (mult (x) (pow x (pred y))) -- a ^ b = a*(a*(b-1))
 genericArithmetic f a x y = if' ((f y) == 0) (x) (a x y)
--- AB/AB2_3.hs
+++ AB/AB2_3.hs
@@ -13,7 +13,24 @@ acyclic      :: Graph -> Bool
 
 -- *** DO NOT MODIFY ABOVE CODE ***
 
-inNeighbors v ((gv,ge) : g) = undefined
-outNeighbors = undefined
-reduction = undefined
-acyclic = undefined
+
+
+inNeighbors v (Graph g) = map fst (filter f g) 
+    where 
+        f x = contains v (snd x) 
+
+outNeighbors v (Graph g) = h (map snd (filter f g))
+    where 
+        f x = v == (fst x) 
+        h x = if' (isEmpty x) [] (unpack x) --checks for empty outneighborlist
+        unpack [x] = x --map(filter()) returns the single list of outneighbors in another list, it has to be unpacked
+
+reduction (Graph g) = Graph (map (f') (filter f g))
+  where
+      f s = not (isEmpty (inNeighbors (fst s) (Graph g)) || isEmpty (outNeighbors (fst s) (Graph g)))
+      f' el = ((fst el), (filter h (snd el)))
+        where 
+            h x = contains x (vertices (Graph(filter f g)))
+
+acyclic (Graph g) = if' (isEmptyGraph (Graph g)) True (if' ((Graph g) == (reduction (Graph g))) False (acyclic (reduction (Graph g))))
+
