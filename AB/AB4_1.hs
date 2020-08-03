0c899393e758104a98b92dda80653edddb70a5ec
= <kris.borrmann@gmail.com>
2020-08-03 12:59:26 +0200

    m3 added to testing; ab 4 1

--- AB/AB4_1.hs
+++ AB/AB4_1.hs
@@ -6,6 +6,7 @@ import AB1_3 (crossList)
 import SPLib.Basic (Bool(True,False), String, trace, undefined, if', not, (&&), (||), (.), (+), (-), (==), (/=))
 import SPLib.List (map, filter, foldl, first, removeFirst, prepend, last, concat, isEmpty, length)
 import SPLib.Tree (Tree(Tree))
+import Testing
 
 
 class FormalLanguage a where
@@ -27,10 +28,49 @@ isTotal :: DFA -> String -> Bool --true iff transition function is defined for a
   
 -- *** DO NOT MODIFY ABOVE CODE ***
 
-statePath = undefined
-stateTree = undefined
+a1 = Testing.m1
+a2 = Testing.m2
+a3 = Testing.m3
+statePath a s i = if' ((length i) == 1) [(nextState a s (first i))] (prepend s (statePath a (nextState a s (first i)) (removeFirst i)))
+traverseNextStates a input state = stateTree a state (removeFirst input)
+stateTree a cur_state input = 
+  if' (isEmpty input) 
+  (
+    Tree cur_state []
+  )(
+    Tree cur_state (map (traverseNextStates a input) (nextStates a cur_state (first input)))
+  )
 
-inLanguageDFA = undefined
-inLanguageNFA = undefined
+teststatePath = statePath a1 "z0" "a"
+tst = stateTree a3 "z0" "001"
 
-isTotal = undefined
+inLanguageDFA a i = isFinalState a (last (statePath a (startState a) (i)))
+recOr a h = 
+  if' (isEmpty h) 
+  (
+    False
+  )(
+    isFinalState a (first h) || recOr a (removeFirst h)
+  )
+
+inLanguageNFA a i = recOr a (nextStateList a i [startState a])
+
+nextStateList :: NFA -> String -> [State] -> [State]
+nextStateList a i z = 
+  if' (length i == 1)
+    (
+      nextList a i z
+    )(
+      nextStateList a (removeFirst i) (nextList a i z)
+    )
+
+nextList a i z = 
+  if' (isEmpty z) 
+  (
+    []
+  )(
+    concat (map (\x -> nextStates a x (first i)) z)
+  )
+tstnxtstlst = nextStateList a3 "001" ["z0"]
+
+isTotal a input = (last (statePath a (startState a) input)) /= "UNDEFINED!"
