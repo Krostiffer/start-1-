f41cd665f66f22088eed2a97676511beea20064c
Leon <leonolmo.lecht@gmail.com>
2020-08-03 10:41:29 +0200

    DFA is finished

--- AB/AB4_1.hs
+++ AB/AB4_1.hs
@@ -7,6 +7,9 @@ import SPLib.Basic (Bool(True,False), String, trace, undefined, if', not, (&&), 
 import SPLib.List (map, filter, foldl, first, removeFirst, prepend, last, concat, isEmpty, length)
 import SPLib.Tree (Tree(Tree))
 
+-- REMOVE AFTER TESTING
+import Testing
+
 
 class FormalLanguage a where
   inLanguage :: a -> String -> Bool
@@ -26,11 +29,16 @@ inLanguageNFA :: NFA -> String -> Bool
 isTotal :: DFA -> String -> Bool --true iff transition function is defined for all 
   
 -- *** DO NOT MODIFY ABOVE CODE ***
+a1 :: DFA
+a1 = Testing.m1
 
-statePath = undefined
+a2 :: NFA
+a2 = Testing.m2
+
+statePath a cur_state input = if' ((length input) == 0) [cur_state] (prepend cur_state (statePath a (nextState a cur_state (first input)) (removeFirst input)))
 stateTree = undefined
 
-inLanguageDFA = undefined
+inLanguageDFA a input = isFinalState a (last (statePath a (startState a) (input)))
 inLanguageNFA = undefined
 
 isTotal = undefined
